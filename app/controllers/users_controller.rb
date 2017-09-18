f7f22f976c054dfb4a638082db4203efadd4fbd7
Amir Nabaei <amir_nabaei@Amirs-MacBook-Air-5.local>
2017-09-17 17:00:03 -0700

    create user models

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  protect_from_forgery with: :null_session
   def index
-
+   @name = User.new
   end
 
   def new
@@ -8,6 +9,12 @@ class UsersController < ApplicationController
 
   def create
 
+   @user = User.new(params.require("user").permit("first_name"))
+   if @user.save
+     render json: { id: @user.id }
+   else
+     render json: { error: @user.errors.full_messages }
+   end
   end
 
   def users_doajax
